bea801d608f90a3b9e853815c3af668467faad77
Carlos Vazquez <flofies@gmail.com>
2021-01-27 09:52:55 -0600

    First Commit
    These are the first scripts (R and Python) that I commit to the Master

---
+++ R_Script_01.r
@@ -0,0 +1,6 @@
+# This is the first R Script written by me for the project:
+
+vector.01 <- c(21, 23, 25, 21, 20, 21, 18, 32, 24, 15, 9)
+class(vector.01)
+
+seq(1, 20, 3)
---
+++ python_script_01.py
@@ -0,0 +1,4 @@
+# This is the first Python script written in RStudio:
+for x in range(0, 20):
+  print(f"El valor de x es ahora: {x}")
+  
